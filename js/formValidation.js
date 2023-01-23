058ac26c56a3181041b516fb865548cd7629e2ad
Carmenvanba <carvanrod94@gmail.com>
2023-01-23 18:58:22 +0100

    Index Template Finished

---
+++ js/formValidation.js
@@ -0,0 +1,8 @@
+//Form Submit
+const element = document.getElementById("submitForm");
+element.addEventListener("click", submitForm);
+
+function submitForm() {
+   // window.location.replace('../html/loreseekers.html');
+    location.replace("../html/loreseekers.html");
+}
